79179aa9b440f234feb53f7a5180d3798cdf89d5
surazregmy <suraz.regmy@gmail.com>
2023-11-10 21:10:24 +0545

    my first program

---
+++ myscript.js
@@ -0,0 +1,18 @@
+function handleButtonOn() {
+  document.getElementById("bulbimage").src = "pic_bulbon.gif";
+}
+
+function handleButtonOff() {
+  document.getElementById("bulbimage").src = "pic_bulboff.gif";
+}
+
+function handleToggle() {
+  var currentImageSource = document.getElementById("bulbimage").src;
+  if (
+    currentImageSource == "file:///D:/NirjalaLearning/website/pic_bulboff.gif"
+  ) {
+    document.getElementById("bulbimage").src = "pic_bulbon.gif";
+  } else {
+    document.getElementById("bulbimage").src = "pic_bulboff.gif";
+  }
+}
